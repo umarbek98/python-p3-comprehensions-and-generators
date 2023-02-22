7b998611c9a43e66aa29462a20228aee44593280
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-02-22 13:04:30 -0500

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [n for n in num_list if n % 2 == 0]
+    return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentance + '!' for sentance in sentence_list]
